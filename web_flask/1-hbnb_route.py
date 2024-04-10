659bce58825f684602b9fb9502e4fcc3b71fe866
Kiwizacxs <brian.deleon2016@outlook.com>
2024-04-10 16:37:31 +0000

    add a new route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+module taht start Flask web applcation
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """
+    This method display an string
+    """
+    return "<p>Hello HBNB!</p>"
+
+app.route("/hbnb", strict_slashes=False)
+def only_hbnb():
+    """
+    This method display an string
+    """
+    return "<p>HBNB</p>"
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
