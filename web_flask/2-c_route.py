b2cd69e52b0886d3b3d4e1749a9a7f6bdd3a4235
Kiwizacxs <brian.deleon2016@outlook.com>
2024-04-10 21:12:07 +0000

    using escape

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+module that contain apps
+"""
+from flask import Flask
+from markupsafe import escape as escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """
+    This method display an string
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def only_hbnb():
+    """
+    This method display an string
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_text(text):
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
