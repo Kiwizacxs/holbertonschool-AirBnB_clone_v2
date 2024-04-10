e35492068abcb373bcb9a7cd4772cd7d5f36373e
Kiwizacxs <brian.deleon2016@outlook.com>
2024-04-10 17:02:20 +0000

    update apps

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 module taht start Flask web applcation
 """
@@ -6,12 +7,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     """
     This method display an string
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,6 +7,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     """
@@ -14,12 +15,14 @@ def hello_HBNB():
     """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def only_hbnb():
     """
     This method display an string
     """
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0")
