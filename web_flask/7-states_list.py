b8c3f0bc53cf74ba4770f98c84e5ca44e158bd0a
Kiwizacxs <brian.deleon2016@outlook.com>
2024-04-14 22:55:21 +0000

    fix

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from flask import Flask
+from models import storage
+from markupsafe import escape as escape
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session():
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_html():
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ = "__main__":
+    app.run(port=5000, host="0.0.0.0")
